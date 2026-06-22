0bca2d4f24e05254af7f3ac8c07c835443356da2
DeveloperKubilay <kubiss52@yahoo.com>
2026-06-22 15:41:58 +0300

    reconnect system added

--- common/src/main/java/org/developerkubilay/safra/p2p/P2pConstants.java
+++ common/src/main/java/org/developerkubilay/safra/p2p/P2pConstants.java
@@ -40,6 +40,8 @@ public final class P2pConstants {
     static final long MAX_PACING_INTERVAL_NANOS = 1_000_000L;
     static final long STUN_REFRESH_MS = 20_000L;
     public static final long RENDEZVOUS_TIMEOUT_MS = 15_000L;
+    static final long RENDEZVOUS_RECONNECT_WINDOW_MS = 120_000L;
+    static final long RENDEZVOUS_RECONNECT_DELAY_MS = 5_000L;
     static final long VOICE_HOST_WAIT_MS = 5_000L;
     public static final int TURN_REQUEST_TIMEOUT_MS = 6_000;
     public static final int TURN_DEFAULT_CREDENTIAL_TTL_SECONDS = 10 * 60;
