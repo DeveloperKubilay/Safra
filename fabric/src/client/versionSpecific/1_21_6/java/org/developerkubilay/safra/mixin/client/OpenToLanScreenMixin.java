10a13ea63293ad247063a5e33a85a84f05ff0d3e
DeveloperKubilay <kubiss52@yahoo.com>
2026-07-17 19:50:45 +0300

    chat msg updated and api changed release

--- fabric/src/client/versionSpecific/1_21_6/java/org/developerkubilay/safra/mixin/client/OpenToLanScreenMixin.java
+++ fabric/src/client/versionSpecific/1_21_6/java/org/developerkubilay/safra/mixin/client/OpenToLanScreenMixin.java
@@ -138,7 +138,11 @@ abstract class OpenToLanScreenMixin extends Screen {
         int tcpPort = this.port;
         this.safra$addClientSystemMessage(Component.translatable("safra.p2p.host.starting"));
         String fixedCode = FabricLanSessionState.isFixedCodeEnabled() ? FabricLanSessionState.getFixedCode() : null;
-        P2pManager.getInstance().startHostingAsync(tcpPort, fixedCode).whenComplete((shareCode, throwable) -> {
+        P2pManager.getInstance().startHostingAsync(tcpPort, fixedCode, () -> this.minecraft.execute(() ->
+            this.safra$addClientSystemMessage(
+                Component.translatable("safra.p2p.host.relay_warning").copy().withStyle(ChatFormatting.YELLOW)
+            )
+        )).whenComplete((shareCode, throwable) -> {
             if (this.minecraft == null) {
                 return;
             }
