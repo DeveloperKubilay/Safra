4525143310fd01695f6dbafe270b55a0010bfe1b
DeveloperKubilay <kubiss52@yahoo.com>
2026-09-19 14:40:03 +0300

    Set the rewritten server name to the world name

--- fabric/src/client/java/org/developerkubilay/safra/client/p2p/P2pManager.java
+++ fabric/src/client/java/org/developerkubilay/safra/client/p2p/P2pManager.java
@@ -209,6 +209,11 @@ public final class P2pManager {
         String localAddress = P2pConstants.LOCAL_PROXY_HOST + ":" + localPort;
         ServerInfo rewritten = new ServerInfo(originalServerInfo.name, localAddress, originalServerInfo.isLocal());
         FabricClientCompat.copyServerInfo(rewritten, originalServerInfo);
+        rewritten.address = localAddress;
+        IntegratedServer integratedServer = MinecraftClient.getInstance().getServer();
+        if (integratedServer != null) {
+            rewritten.name = integratedServer.getSaveProperties().getLevelName();
+        }
         return new RewriteResult(ServerAddress.parse(rewritten.address), rewritten);
     }
 
--- forge/src/main/java/org/developerkubilay/safra/client/p2p/P2pManager.java
+++ forge/src/main/java/org/developerkubilay/safra/client/p2p/P2pManager.java
@@ -209,6 +209,11 @@ public final class P2pManager {
         String localAddress = P2pConstants.LOCAL_PROXY_HOST + ":" + localPort;
         ServerData rewritten = new ServerData(originalServerInfo.name, localAddress, originalServerInfo.isLan());
         rewritten.copyFrom(originalServerInfo);
+        rewritten.ip = localAddress;
+        IntegratedServer integratedServer = Minecraft.getInstance().getSingleplayerServer();
+        if (integratedServer != null) {
+            rewritten.name = integratedServer.getWorldData().getLevelName();
+        }
         return new RewriteResult(ServerAddress.parseString(rewritten.ip), rewritten);
     }
 
